44bc86366fb40e0f94ba4f4195278c741b79dcd5
Janaina Andrade <janalimapereira.jalp@gmail.com>
2022-11-03 21:20:03 -0300

    Arrays e Estruturas de repetçõesi

---
+++ arrays e repeticoes/arrays.js
@@ -0,0 +1,38 @@
+/*const alunos = ['João', 'Victor', 'Maria', 'Janaina']
+
+//alunos.push('Janaina')
+
+//ou
+
+//alunos[4] = 'Vinicius'
+
+console.log(alunos);
+
+//pop imprime a ultima posição do Array
+console.log(alunos.pop());
+// shift imprime a primeira posição do Array
+console.log(alunos.shift());
+
+console.log(alunos) */
+
+/*const notas = []
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+
+const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
+
+console.log(soma / 5); */
+
+
+//Estruturas de repetição
+
+const nome = 'Janaina Andrade Lima Pereira'
+for (let i = 0; i < nome.length; i++) {
+    const letra = nome[i];
+      console.log(nome[i]); 
+} 
+
